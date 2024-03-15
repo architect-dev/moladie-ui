78c232a45191139cb7ffa506f13712d3363cd3b9
halftone-dev <halftonedev@gmail.com>
2024-03-15 22:04:24 +0100

    Add dist

---
+++ dist/assets/Loser-CQEpOVnS.js
@@ -0,0 +1,1 @@
+import{j as e,H as s}from"./index-C2yd2dwO.js";const r=()=>e.jsx(s,{style:{fontSize:"100px"},children:"FUCK YOU"});export{r as default};
---
+++ dist/assets/NotFound-fTjCEPqm.js
@@ -0,0 +1,7 @@
+import{s as t,j as e,P as s,H as n,T as o,S as i,L as a}from"./index-C2yd2dwO.js";const r=t.div`
+	align-items: center;
+	display: flex;
+	flex-direction: column;
+	height: calc(100vh - 264px);
+	justify-content: center;
+`,l=()=>e.jsx(s,{children:e.jsxs(r,{children:[e.jsx(n,{style:{fontSize:"100px"},children:"404"}),e.jsx(o,{monospace:!0,mb:"48px",children:"Page Not Found"}),e.jsx(i,{as:a,to:"/",replace:!0,children:"HOME"})]})});export{l as default};
